09b911ad0452006bc7d344b2b93055118b037ebf
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-02 16:48:33 -0600

    first test passing

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,12 @@
 export default class DistrictRepository {
+  constructor (data) {
+    this.data = (data) => {
+      console.log(data);
+    };
+  }
+
+  // data() {
+  //   return this.storedData;
+  // }
 
 }
