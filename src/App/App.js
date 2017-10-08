4f3056b933c2c3369e16e2c4331377d02466f93b
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-08 17:19:22 -0600

    created controls and moved component func over into it

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import Helper from '../helper';
 import CardContainer from '../CardContainer/CardContainer';
 import CompareContainer from '../CompareContainer/CompareContainer';
 import DataButton from '../DataButton/DataButton';
+import Controls from '../Controls/Controls';
 import kinderData from '../../data/kindergartners_in_full_day_program';
 import hsGradRatesData from '../../data/high_school_graduation_rates';
 import thirdGradeTestData from '../../data/3rd_grade_tests';
@@ -22,14 +23,11 @@ class App extends Component {
 
     this.helper = new Helper(kinderData);
     this.kinderData =  this.helper.kinderData;
-    // this.kinderdata is just our data variable, kinder data on 13 is our data source
     this.state = {
       school: null,
       years: [],
-      inputValue: "",
       numberOfSelected: 0,
       schoolsSelected: [],
-      possibleMatches: [],
       dataSet: (new Helper(kinderData)).kinderData,
       dataArray: [
         {name: "HS Graduation Rates", districtData: hsGradRatesData},
@@ -46,47 +44,13 @@ class App extends Component {
       ]
     };
 
-    this.onFindClick = this.onFindClick.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleCompareClick = this.handleCompareClick.bind(this);
+    this.handleClick          = this.handleClick.bind(this);
+    this.handleCompareClick   = this.handleCompareClick.bind(this);
     this.comparedCardAverages = this.comparedCardAverages.bind(this);
-    this.changeDataSet = this.changeDataSet.bind(this);
+    this.changeDataSet        = this.changeDataSet.bind(this);
   }
-
-  onFindClick(event) {
-    let school = this.helper.findByName(this.state.inputValue);
-
-    if (typeof school === 'object') {
-      let years = [];
-
-      for (let year in school.data) {
-        if (typeof year === 'string') {
-          years.push(school.data[year]);
-        }
-      }
-      this.setState({ school: school, years: years });
-    }
-  }
-
-  onChange(event) {
-    let val = event.target.value;
-    this.setState({
-      inputValue: val,
-      possibleMatches: val ? this.helper.findAllMatches(val) : []
-    });
-
-  }
-
-  // schoolsSelected is not in the array yet!
 
   handleClick(location){
-
-    //this code is breaking our shit/stuff
-
-    // const locationFound = this.state.schoolsSelected.find((location) => {
-    //   return location;
-    // });
     const numSelected = this.state.numberOfSelected;
     let schoolIndex = this.state.schoolsSelected.indexOf(location);
 
@@ -126,22 +90,13 @@ class App extends Component {
     this.helper.findAverage(schoolName, true);
   }
 
-  // this.helper.findAvereage
-  // this.helper.compareDistrictAverages
-  // schools is array of two objects w school data
-
   comparedCardAverages(schools) {
-
     const array = schools.map((school)=>{
       return school.location;
     });
-
     if (array.length === 2) {
       return this.helper.compareDistrictAverages(array[0], array[1]);
     }
-
-    // this.helper.compareDistrictAverages(array[0], array[1])
-
   }
 
   changeDataSet(dataSet) {
@@ -155,56 +110,14 @@ class App extends Component {
   render() {
     return (
       <div className="app-render">
-        <div className="data-button-container">
-          {this.state.dataArray.map((element, index)=>{
-            return (
-              <DataButton
-                changeDataSet={this.changeDataSet}
-                dataSet={element.districtData}
-                dataName={element.name}
-                key={index}
-              />
-            );
-          })}
-        </div>
-        <input
-          className="search-input"
-          placeholder="Search for School Here"
-          type="text"
-          value={ this.state.inputValue }
+        <Controls
+          dataArray={this.state.dataArray}
+          changeDataSet={this.changeDataSet}
           onChange={this.onChange}
+          helper={this.helper}
+          handleClick={this.handleClick}
         />
-        <ul className="search-list">
-          {this.state.possibleMatches.map((location, index, event) => {
-            return (
-              <li
-                href="location"
-                className="list-items"
-                key={index}
-              >
-                <button
-                  onClick={() => {
-                    this.handleClick(location.location);
-                  }}
-                >{location.location}
-                </button>
-
-              </li>
-            );
-          })}
-        </ul>
-        <h1>{this.state.school
-          ? this.state.school.location
-          : ""}
-        </h1>
-        {this.state.years.map((year, index) => {
-          return (
-            <h2 key={ index }>{ year }</h2>
-          );
-        })}
         <CompareContainer
-          // handleClick={this.handleClick}
-
           selectedCards={this.selectedCards.bind(this)}
           schoolsSelected={this.state.schoolsSelected}
           compareData={this.comparedCardAverages}
@@ -215,10 +128,8 @@ class App extends Component {
           kinderData={this.state.dataSet}
           handleClick={this.handleClick}
           numberOfSelected={this.state.numberOfSelected}
-          // cardAverages={this.cardAverages}
         />
       </div>
-
     );
   }
 }
