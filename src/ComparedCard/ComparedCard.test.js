a5f8c66290b17bc474a1364b679423dad58db53d
johnmboudreaux <jhnbdrx@gmail.com>
2017-10-08 20:45:48 -0600

    comared card tests were failing. added some mock functions and updated snapshot

--- src/ComparedCard/ComparedCard.test.js
+++ src/ComparedCard/ComparedCard.test.js
@@ -1,12 +1,14 @@
 import ReactDom from 'react-dom';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import ComparedCard from './ComparedCard';
 
 describe('Compare card', ()=>{
   it('should match the Compare card snapshot', () => {
     const renderedComponent = shallow(
       <ComparedCard
+        changeDataSet={jest.fn()}
+        handleClick={jest.fn()}
         selectedCards={jest.fn()}
         compareData={jest.fn()}
       />
